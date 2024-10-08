e0097db1b19bd52ea101658290bfcdfb9e96297c
luis david <11556208+luisdamora@users.noreply.github.com>
2024-10-08 09:11:50 -0500

    feat(config): agregar configuración de sitio y base
    - Configurar el sitio en `astro.config.ts` para apuntar a la URL del proyecto.
    - Establecer la base para la implementación en GitHub Pages.

--- astro.config.ts
+++ astro.config.ts
@@ -23,6 +23,8 @@ const whenExternalScripts = (items: (() => AstroIntegration) | (() => AstroInteg
 
 export default defineConfig({
   output: 'static',
+  site: 'https://luisdamora.github.io',
+  base: '/caprinosol_web',
 
   integrations: [
     tailwind({
